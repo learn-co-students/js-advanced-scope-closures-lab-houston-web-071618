f2f05f3b093b125371719addcacab81208b5ac4b
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-09-03 12:37:14 -0500

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,13 @@ function produceTipCalculator(percentTip){
     return amount*percentTip;
   }
 }
+
+function createDriver(){
+  let driverId = 0;
+  return class {
+    constructor (name){
+      this.name = name;
+      this.id = ++driverId;
+    }
+  };
+}
